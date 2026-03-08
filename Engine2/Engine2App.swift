cba0a236b49939789a5d4d78937e4917d8ba6e97
twistinside <twistinside@users.noreply.github.com>
2026-03-08 14:13:53 -0700

    Initial Commit

---
+++ Engine2/Engine2App.swift
@@ -0,0 +1,17 @@
+//
+//  Engine2App.swift
+//  Engine2
+//
+//  Created by Karl Groff on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Engine2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Engine2Tests/Engine2Tests.swift
@@ -0,0 +1,19 @@
+//
+//  Engine2Tests.swift
+//  Engine2Tests
+//
+//  Created by Karl Groff on 3/8/26.
+//
+
+import Testing
+@testable import Engine2
+
+struct Engine2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
