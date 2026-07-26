cfbf964cc4173d952e07e2de91e02ca9c2b8ee50
twistinside <twistinside@users.noreply.github.com>
2026-07-25 19:27:39 -0700

    Organize Comparable conformances

--- Engine2/Input Runtime/InputRevision.swift
+++ Engine2/Input Runtime/InputRevision.swift
@@ -2,19 +2,11 @@
 ///
 /// A new session starts whenever the runtime is restarted. Sequence numbers
 /// order publications within that session.
-nonisolated struct InputRevision: Equatable, Comparable, Sendable {
+nonisolated struct InputRevision: Equatable, Sendable {
     let session: UInt64
     let sequence: UInt64
 
     static let initial = InputRevision(session: 0, sequence: 0)
-
-    static func < (lhs: InputRevision, rhs: InputRevision) -> Bool {
-        if lhs.session == rhs.session {
-            lhs.sequence < rhs.sequence
-        } else {
-            lhs.session < rhs.session
-        }
-    }
 
     func advanced() -> InputRevision {
         precondition(sequence < .max, "Input revision sequence exhausted")
@@ -24,5 +16,15 @@ nonisolated struct InputRevision: Equatable, Comparable, Sendable {
     func startingNextSession() -> InputRevision {
         precondition(session < .max, "Input revision session exhausted")
         return InputRevision(session: session + 1, sequence: 0)
+    }
+}
+
+extension InputRevision: Comparable {
+    static func < (lhs: InputRevision, rhs: InputRevision) -> Bool {
+        if lhs.session == rhs.session {
+            lhs.sequence < rhs.sequence
+        } else {
+            lhs.session < rhs.session
+        }
     }
 }
